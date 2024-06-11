753e4d22b0a617939c7deda9afb81d6151e991b3
phil <philipp@itell.solutions>
2024-06-11 08:03:56 +0200

    #main: initial commit

---
+++ android/app/src/main/kotlin/com/example/paged_data_table_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.paged_data_table_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import "package:flutter_localizations/flutter_localizations.dart";
+import 'package:form_builder_validators/localization/l10n.dart';
+import 'package:paged_datatable/l10n/generated/l10n.dart';
+
+import 'generated/l10n.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        PagedDataTableLocalization.delegate,
+        S.delegate,
+        FormBuilderLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    const String overriddenLocale = "de";
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(S.of(context).language),
+            Text(PagedDataTableLocalization.of(context).nextPageButtonText),
+            Localizations.override(
+                context: context,
+                // NOTE: if you change this line to "en", "it" or "es" then the text is shown in the correct language
+                // "de" is not working
+                locale: const Locale(overriddenLocale),
+                child: Builder(builder: (context) {
+                  return Column(
+                    children: [
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      const Text("With overridden context and locale set to: $overriddenLocale"),
+                      Text(S.of(context).language),
+                      Text(PagedDataTableLocalization.of(context).nextPageButtonText),
+                      Text(FormBuilderLocalizations.of(context).emailErrorText),
+                    ],
+                  );
+                })),
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
